b7f7701758cc9b14c89c50696af604647acefbe4
Thomas Theiner <thomas.theiner@gmx.de>
2017-03-17 17:56:51 +0100

    Basic authentication

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var routes = require('./routes');
 
+var auth = require('http-auth');
+var basic = auth.basic({
+      realm: "Web."
+   }, (username, password, callback) => {
+      var passed = (username === "thomas.theiner@gmx.de" && password === "ngc64738");
+      if(!passed) console.log("Authentication failed");
+      callback(passed);
+   }
+);
+
 var app = express();
 
 var port = process.env.PORT || 8080;
@@ -14,8 +24,8 @@ app.use(express.query());
 app.get('/', routes.index);
 
 app.get('/git/:reponame/info/refs', routes.getInfoRefs);
-app.post('/git/:reponame/git-receive-pack', routes.postReceivePack);
-app.post('/git/:reponame/git-upload-pack', routes.postUploadPack);
+app.post('/git/:reponame/git-receive-pack', auth.connect(basic), routes.postReceivePack);
+app.post('/git/:reponame/git-upload-pack', auth.connect(basic), routes.postUploadPack);
 
 app.listen(port, () => {
    console.log('Git Server listening on port ' + port + ' ...');
