8f9a3ee6ed7a6dae8a568f411b6cc6f6b83bc22c
Thomas Theiner <thomas.theiner@gmx.de>
2017-03-20 12:58:13 +0100

    Cleansed the index.js example file

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 var git = require('./git-server-windows');
 
-var thomas = new git.User({
-   username: "thomas.theiner@gmx.de",
-   password: "ngc64738"
+var myUser = new git.User({
+   username: "xxxxxxxxxxxxxxxxx",
+   password: "yyyyyyyyy"
 });
 
 git.server({
-   defaultUsers : [thomas],
-   repositories: {
-      "node_remotegit" : []
-   }
+   defaultUsers : [myUser]
 });
